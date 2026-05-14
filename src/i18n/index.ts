a4aca89c06f8e0ad588b29ef9708d8fcff1c380e
Stefano <stefanodelmontedeveloper@gmail.com>
2026-05-14 09:27:31 +0200

    refactor: remove unused i18n module

--- src/i18n/index.ts
+++
@@ -1,6 +0,0 @@
-import { es } from './es';
-import { en } from './en';
-
-export const translations = { es, en };
-export type Lang = keyof typeof translations;
-export const defaultLang: Lang = 'es';
